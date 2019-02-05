761b3747dd7238300288405e1226f93b2cfe1149
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-02-05 14:27:31 -0800

    added rollup packages to package.json, rollup.config file

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs';
+import node from "rollup-plugin-node-resolve";
+import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const copyright = `// @observablehq/parser v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+
+export default [
+{
+  input: "src/index.js",
+  plugins: [
+    node(),
+    commonjs(),
+    terser({
+      output: {preamble: copyright},
+      mangle: {reserved: ["RequireError"]}
+    })
+  ],
+  output: {
+    format: "umd",
+    extend: true,
+    name: "observablehq",
+    file: "dist/parser.js"
+  }
+}
+];
