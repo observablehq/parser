9a59ca5fb8b4d224601db80aea8e19df1f896875
Tom MacWright <tom@macwright.org>
2019-09-20 10:19:25 -0700

    Remove SpreadProperty walker because we now use acorn’s built-in support which generates SpreadElement, which is handled natively by acorn-walk

--- src/walk.js
+++ src/walk.js
@@ -2,9 +2,6 @@ import {make} from "acorn-walk";
 
 export default make({
   Import() {},
-  SpreadProperty(node, st, c) {
-    c(node.argument, st, "Expression");
-  },
   ObjectPattern(node, st, c) {
     const list = node.properties;
     for (const prop of list) {
