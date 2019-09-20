09958d906374805e49ef761382572bbd74faf0ea
Tom MacWright <tom@macwright.org>
2019-09-20 10:44:31 -0700

    Remove ObjectPattern walker. This is also built-in

--- src/walk.js
+++ src/walk.js
@@ -2,12 +2,6 @@ import {make} from "acorn-walk";
 
 export default make({
   Import() {},
-  ObjectPattern(node, st, c) {
-    const list = node.properties;
-    for (const prop of list) {
-      c(prop.value || prop.argument, st, "Pattern");
-    }
-  },
   ViewExpression(node, st, c) {
     c(node.id, st, "Identifier");
   },
